7bde2e34829991c3278a22689d500fae9e0c6f05
Ilya <kupriyanov_2002@mail.ru>
2021-07-03 00:49:26 +0300

    added main file

---
+++ Draw-poker/Draw-poker/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+//структура отвечающая за одну карту
+struct card {
+	char* suit_card;
+	char* nomanal_card;
+};
+
+int main() {
+	const char* suit[] = { "Hearts", "Diamond", "Spades", "Club" }; //указатели на названия масти карт
+	const char* nominal[] = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+							  "Jack", "Queen", "King", "Ace" }; //указатели на номиналы карт
+
+
+	return 0;
+}
