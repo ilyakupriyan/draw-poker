cc33003a58ffa96beb255d64673c2f5c6c57f205
Ilya <kupriyanov_2002@mail.ru>
2021-07-04 10:46:52 +0300

    Added functions initialization and shuffling of deck

--- Draw-poker/Draw-poker/cards.cpp
+++ Draw-poker/Draw-poker/cards.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <time.h>
 #include <iomanip>
 #include <conio.h>
 
@@ -12,7 +13,7 @@ struct card {
 };
 
 const char* suit[] = { "Hearts", "Diamond", "Spades", "Club" }; //указатели на названия масти карт
-const char* nominal[] = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+const char* nominal[] = { "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
 						  "Jack", "Queen", "King", "Ace" }; //указатели на номиналы карт
 
 //функция вывода карт одного игрока
@@ -25,9 +26,59 @@ void printCards(int const cards[][2]) {
 
 //тест на правильный вывод карт одного игрока
 void testPrintCards() {
+	cout << "Test printCards is running" << endl;
 	cout << "There should be: three of hearts, five of diamond, six of spades, eight, jack of club, ace of spades" << endl;
+
 	int cards[5][2] = { {1, 0}, {3, 1}, {4, 2}, {9, 3}, {12, 2} }; //тройка черви, пять бубны, шесть пики, валет крести, туз пики
 	printCards(cards);
+
 	cout << "Enter any key to continue";
 	_getch();
+}
+
+//функция инициализации колоды в -1 для дальнейшего тасования
+void deckInit(int deck[][2]) {
+	for (int card = 0; card <= 51; card++) {
+		deck[card][0] = -1;
+		deck[card][1] = -1;
+	}
+}
+
+//функция перемешивания колоды карт из 52 карт
+void deckShuffle(int deck[][2]) {
+	srand(time(NULL));
+
+	int suit = 0, nom = 0;
+
+	for (int card = 0; card <= 51; card++) {
+		suit = rand() % 3;
+		nom = rand() % 12;
+
+		while (deck[card][0] == nom || deck[card][1] == suit) {
+			suit = rand() % 3;
+			nom = rand() % 12;
+		}
+		deck[card][0] = nom;
+		deck[card][1] = suit;
+	}
+}
+
+//функция теста deckShuffle()
+int testDeckShuffle() {
+
+	//инициализация и тасование колоды карт
+	int deck[52][2];
+	
+	for (int crd = 0; crd <= 51; crd++) {
+		deck[crd][0] = -2;
+		deck[crd][1] = -2;
+	}
+
+	deckShuffle(deck);
+
+	for (int i = 0; i <= 51; i++) {
+		if (deck[i][0] == -2 || deck[i][1] == -2)
+			return -1;
+	}
+	return 0;
 }
--- Draw-poker/Draw-poker/cards.h
+++ Draw-poker/Draw-poker/cards.h
@@ -3,5 +3,8 @@
 
 void printCards(int const cards[][2]);
 void testPrintCards();
+void deckInit(int deck[][2]);
+void deckShuffle(int deck[][2]);
+int testDeckShuffle();
 
 #endif
