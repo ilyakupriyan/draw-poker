c3d568dc080c0eaf2d52bed57074c27e5b86eb3b
Ilya <kupriyanov_2002@mail.ru>
2021-07-04 11:37:04 +0300

    Added initialization and removal of bots

--- Draw-poker/Draw-poker/main.cpp
+++ Draw-poker/Draw-poker/main.cpp
@@ -5,12 +5,43 @@
 int main() {
 	std::string comb, cont = "Yes";
 
-	//инициализация колоды карт
+	//определение колоды карт
 	int deck[52][2]; //первый индекс - номер карты, второй: 0 - масть, 1 - номинал
-	deckInit(deck); //инициализация колоды в -1
+
+	int user[5][2]; //карты пользователя
+	int*** card_bots; //указатель на карты ботов
+	int num_bot;
 
 	while (cont != "NO") {
+		//инициализация и тасование колоды карт
+		deckInit(deck);
+		deckShuffle(deck);
 
+		//ввод количества ботов
+		std::cout << "Number of bots: ";
+		std::cin >> num_bot;
+		while (num_bot < 1 || num_bot > 4) {
+			std::cout << "Number of bots should be between 1 and 4" << std::endl;
+			std::cin >> num_bot;
+		}
+
+		//создание ботов
+		card_bots = new int**[num_bot];
+		for (int bot = 0; bot < num_bot; bot++) {
+			card_bots[bot] = new int* [5];
+			for (int cards = 0; cards < 5; cards++)
+				card_bots[bot][cards] = new int[2];
+		}
+
+
+
+
+		//освобождение памяти, выделенную под ботов, после окончание игры
+		for (int bot = 0; bot < num_bot; bot++) {
+			for (int cards = 0; cards < 5; cards++)
+				delete card_bots[bot][cards];
+			delete card_bots[bot];
+		}
 	}
 	
 	return 0;
